3104fcf9d9add8cac073bf6d027a1721098765bc
Mateus Pragana <mateuspragana@gmail.com>
2022-08-29 19:58:50 -0300

    Criando estilizações Globais da Aplicação

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	body, 
+  input, 
+  textarea, 
+  button {
+		font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+	}
+`
