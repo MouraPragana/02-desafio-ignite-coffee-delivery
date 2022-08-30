8cbfa94dd71e23f502a07744ee651a035a17bba6
Mateus Pragana <mateuspragana@gmail.com>
2022-08-30 00:16:12 -0300

    Criando o default layout e suas estilizações

--- src/layouts/defaultLayout/index.tsx
+++ src/layouts/defaultLayout/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import { Header } from '../../components/header/index'
+import { LayoutContainer } from './styles'
 
 export function DefaultLayout() {
   return (
-    <>
-      <header>Menu</header>
+    <LayoutContainer>
+      <Header />
       <Outlet />
-    </>
+    </LayoutContainer>
   )
 }
---
+++ src/layouts/defaultLayout/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+
+  max-width: 90rem;
+  background-color: blue;
+  padding: 0 10rem;
+  margin: 0 auto;
+`
