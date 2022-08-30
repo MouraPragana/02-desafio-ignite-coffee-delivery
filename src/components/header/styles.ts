afc964b607d593abf64672d0d69e3ef8abe73de0
Mateus Pragana <mateuspragana@gmail.com>
2022-08-30 01:48:03 -0300

    Estilizando o contador do card

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -33,9 +33,27 @@ export const LocationCartContainer = styled.div`
   a {
     display: flex;
     align-items: center;
+    position: relative;
     background-color: ${(props) => props.theme['yellow-light']};
     padding: 8px;
     border-radius: 6px;
+    strong {
+      display: flex;
+      justify-content: center;
+      position: absolute;
+      top: 0;
+      right: 0;
+      font-size: 0.75rem;
+      min-width: 20px;
+      min-height: 16px;
+      padding: 0.2rem;
+      border-radius: 50%;
+      margin-top: -8px;
+      margin-right: -8.35px;
+
+      color: ${(props) => props.theme.white};
+      background-color: ${(props) => props.theme['yellow-dark']};
+    }
     img {
       height: 22px;
       background-color: transparent;
