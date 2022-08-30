77527f9b642725fef0f666b239b01005fce20b89
Mateus Pragana <mateuspragana@gmail.com>
2022-08-30 00:15:47 -0300

    Acrescentando as cores do defaultTheme do projeto

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,9 @@
+export const defaultTheme = {
+  'purple-light': '#EBE5F9',
+  purple: '#8047F8',
+  'purple-dark': '#4B2995',
+
+  'yellow-light': '#F1E9C9',
+  yellow: '#DBAC2C',
+  'yellow-dark': '#C47F17',
+}
