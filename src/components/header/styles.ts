b8b34f08b0a6d6099938bff15f2b30063f5e7915
Mateus Pragana <mateuspragana@gmail.com>
2022-08-30 00:51:45 -0300

    Removendo os espaços da estilização do header

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -4,10 +4,8 @@ export const HeaderContainer = styled.header`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-
   height: 6.5rem;
   align-items: center;
-
   img {
     height: 2.5rem;
   }
@@ -17,32 +15,27 @@ export const LocationCartContainer = styled.div`
   display: flex;
   flex-direction: row;
   gap: 0.75rem;
-
   span {
     display: flex;
     align-items: center;
     font-weight: bold;
+    font-size: 0.875rem;
     color: ${(props) => props.theme['purple-dark']};
     background-color: ${(props) => props.theme['purple-light']};
     padding: 10px 8px;
     border-radius: 6px;
     gap: 4px;
-
     img {
       height: 22px;
       background-color: transparent;
     }
   }
-
   a {
     display: flex;
     align-items: center;
-
     background-color: ${(props) => props.theme['yellow-light']};
-
     padding: 8px;
     border-radius: 6px;
-
     img {
       height: 22px;
       background-color: transparent;
