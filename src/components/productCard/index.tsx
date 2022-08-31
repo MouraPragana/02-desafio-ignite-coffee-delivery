f0bf4ec955a74eab7c4a67619bab1ab2d8d685f3
Mateus Pragana <mateuspragana@gmail.com>
2022-08-31 20:46:15 -0300

    Iniciando a criação do product card component

---
+++ src/components/productCard/index.tsx
@@ -0,0 +1,17 @@
+export function ProductCard() {
+  return (
+    <div>
+      <p>Imagem card</p>
+      <p>Tradicional</p>
+      <p>Expresso Tradicional</p>
+      <p>O tradicional café feito com água quente e grãos moídos</p>
+      <div>
+        <span>R$ 9,90</span>
+        <button>-</button>
+        <span>1</span>
+        <button>+</button>
+        <span>carrinho</span>
+      </div>
+    </div>
+  )
+}
