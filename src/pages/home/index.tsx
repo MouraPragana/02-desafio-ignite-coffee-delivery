a1e013a121f7e14d5a7c71d12266910485913afb
Mateus Pragana <mateuspragana@gmail.com>
2022-08-31 20:46:02 -0300

    Fragmentando a página home em compoentes WelcomeSection e ProductSection

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,54 +1,11 @@
-import {
-  HomeSection,
-  MainContent,
-  Title,
-  SubTitle,
-  AdditionalContent,
-  TextContent,
-} from './styles'
-import imgHome from '../../assets/imgHome.svg'
-import cartAdditionalContent from '../../assets/cartAdditionalContent.svg'
-import packAdditionalContent from '../../assets/packAdditionalContent.svg'
-import deliverAdditionalContent from '../../assets/deliverAdditionalContent.svg'
-import coffeeAdditionalContent from '../../assets/coffeeAdditionalContent.svg'
+import { WelComeSection } from './components/welcomeSection'
+import { ProductsSection } from './components/productsSection'
 
 export function Home() {
   return (
-    <HomeSection>
-      <MainContent>
-        <TextContent>
-          <Title>Encontre o café perfeito para qualquer hora do dia</Title>
-          <SubTitle>
-            Com o Coffe Delivery você recebe seu café onde estiver, a qualquer
-            hora
-          </SubTitle>
-          <AdditionalContent>
-            <span>
-              <img src={cartAdditionalContent} alt="cartAdditionalContent" />
-              Compra simples e segura
-            </span>
-            <span>
-              <img src={packAdditionalContent} alt="packAdditionalContent" />
-              Embalagem mantém o café intacto
-            </span>
-            <span>
-              <img
-                src={deliverAdditionalContent}
-                alt="deliverAdditionalContent"
-              />
-              Entrega rápida e rastreada
-            </span>
-            <span>
-              <img
-                src={coffeeAdditionalContent}
-                alt="coffeeAdditionalContent"
-              />
-              O café chega fresquinho até você
-            </span>
-          </AdditionalContent>
-        </TextContent>
-        <img src={imgHome} alt="" />
-      </MainContent>
-    </HomeSection>
+    <>
+      <WelComeSection />
+      <ProductsSection />
+    </>
   )
 }
