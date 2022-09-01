b8ccfaf0d60ee15c550dbdc5c588bee59d85280e
Mateus Pragana <mateuspragana@gmail.com>
2022-09-01 19:49:17 -0300

    Realizando algumas alterações com chamada do data.json no productsection

--- src/pages/home/components/productsSection/index.tsx
+++ src/pages/home/components/productsSection/index.tsx
@@ -1,26 +1,41 @@
+import { useEffect, useState } from 'react'
 import { ProductCard } from '../../../../components/productCard'
-
 import { CoffeeContent, MainContent, Title } from './styles'
 
+import dataJSON from '../../../../data.json'
+
+interface dataProps {
+  id: string
+  tags: string[]
+  title: string
+  description: string
+  price: number
+  imageUrl: string
+}
+
 export function ProductsSection() {
+  const [data, setDate] = useState<dataProps[]>([])
+
+  useEffect(() => {
+    setDate(dataJSON)
+  }, [data])
+
   return (
     <MainContent>
       <Title>Nossos Cafés</Title>
       <CoffeeContent>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {data.map((coffee) => {
+          return (
+            <ProductCard
+              key={coffee.id}
+              tags={coffee.tags}
+              title={coffee.title}
+              description={coffee.description}
+              price={coffee.price}
+              imageUrl={coffee.imageUrl}
+            />
+          )
+        })}
       </CoffeeContent>
     </MainContent>
   )
