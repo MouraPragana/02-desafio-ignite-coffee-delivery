ee1c9e9cd289a62dc12a55ff7eb17b3cf180325c
Mateus Pragana <mateuspragana@gmail.com>
2022-09-02 20:55:58 -0300

    criando actions do reducers coffee

---
+++ src/reducers/coffee/actions.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { Product } from './reducer'
+
+export enum ActionTypes {
+  ADD_NEW_PRODUCT = 'ADD_NEW_PRODUCT',
+  REMOVE_PRODUCT = 'REMOVE_PRODUCT',
+}
+
+export function newProduct(newProduct: Product) {
+  return {
+    type: ActionTypes.ADD_NEW_PRODUCT,
+    payload: {
+      newProduct,
+    },
+  }
+}
+
+export function removeProduct(product: Product) {
+  return {
+    type: ActionTypes.REMOVE_PRODUCT,
+    payload: {
+      product,
+    },
+  }
+}
